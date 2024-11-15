f912dd0a50042f5dada7f884c401fcddbb5fca9c
Jackesque <tientranquang123@gmail.com>
2024-11-16 01:12:51 +0700

    log user data

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,207 +1,204 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
+import datetime as dt
 
-"""Simple inline keyboard bot with multiple CallbackQueryHandlers.
+from warnings import filterwarnings
+from telegram.warnings import PTBUserWarning
 
-This Bot uses the Application class to handle the bot.
-First, a few callback functions are defined as callback query handler. Then, those functions are
-passed to the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Example of a bot that uses inline keyboard that has multiple CallbackQueryHandlers arranged in a
-ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line to stop the bot.
-"""
-import logging
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Application,
     CallbackQueryHandler,
     CommandHandler,
-    ContextTypes,
     ConversationHandler,
 )
+from constants import TOKEN
+import os
+import pandas as pd
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+
+filterwarnings(
+    action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
 
-logger = logging.getLogger(__name__)
-
-# Stages
 START_ROUTES, END_ROUTES = range(2)
-# Callback data
-ONE, TWO, THREE, FOUR = range(4)
+COUNTRIES, PHONE_NUMBER, END = ["Countries", "Phone Number", "End"]
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Send message on `/start`."""
-    # Get user that sent /start and log his name
-    user = update.message.from_user
-    logger.info("User %s started the conversation.", user.first_name)
-    # Build InlineKeyboard where each button has a displayed text
-    # and a string as callback_data
-    # The keyboard is a list of button rows, where each row is in turn
-    # a list (hence `[[...]]`).
-    keyboard = [
-        [
-            InlineKeyboardButton("1", callback_data=str(ONE)),
-            InlineKeyboardButton("2", callback_data=str(TWO)),
-        ]
+async def start(update, context):
+    message = update.message
+    user = message.from_user
+
+    timestamp = dt.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    user_id = str(user.id)
+    username = user.username
+    first_name = user.first_name
+    last_name = user.last_name
+
+    file_exists = os.path.exists(os.path.join(os.getcwd(), "user_info.xlsx"))
+    index_columns = [
+        "Timestamp",
+        "User Id",
+        "Username",
+        "First Name",
+        "Last Name",
+        "Phone Number",
+        "Broker UID",
+        "Broker Full Name",
+        "Country",
+        "Account Balance (USD)",
+        "Email (for VIP invitation)",
     ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    # Send message with text and appended InlineKeyboard
-    await update.message.reply_text(
-        "Start handler, Choose a route", reply_markup=reply_markup
-    )
-    # Tell ConversationHandler that we're in state `FIRST` now
-    return START_ROUTES
+    if not file_exists:
+        df = pd.DataFrame(columns=index_columns)
+        df.to_csv("users_data (development).csv", index=False)
+    df = pd.read_csv("users_data (development).csv")
+    if user_id not in set(df["User Id"].values):
+        df = pd.concat(
+            [
+                df,
+                pd.DataFrame(
+                    [[timestamp, user_id, username, first_name, last_name]],
+                    columns=index_columns[:5],
+                ),
+            ]
+        )
+        df.to_csv("users_data (development).csv", index=False)
+        df.to_excel("users_data.xlsx", index=False)
 
-
-async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Prompt same text & keyboard as `start` does but not as new message"""
-    # Get CallbackQuery from Update
-    query = update.callback_query
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    await query.answer()
-    keyboard = [
+    reply_markup = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton("1", callback_data=str(ONE)),
-            InlineKeyboardButton("2", callback_data=str(TWO)),
+            [
+                InlineKeyboardButton("Viet Nam", callback_data="Viet Nam"),
+                InlineKeyboardButton("USA", callback_data="USA"),
+                InlineKeyboardButton("Other", callback_data="Other"),
+            ]
         ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    # Instead of sending a new message, edit the message that
-    # originated the CallbackQuery. This gives the feeling of an
-    # interactive menu.
-    await query.edit_message_text(
-        text="Start handler, Choose a route", reply_markup=reply_markup
     )
-    return START_ROUTES
-
-
-async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons"""
-    query = update.callback_query
-    await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("3", callback_data=str(THREE)),
-            InlineKeyboardButton("4", callback_data=str(FOUR)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="First CallbackQueryHandler, Choose a route", reply_markup=reply_markup
+    await message.reply_text(
+        text="What is your country?", reply_markup=reply_markup
     )
-    return START_ROUTES
+    return COUNTRIES
 
 
-async def two(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons"""
+async def phone_number(update, context):
     query = update.callback_query
     await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("1", callback_data=str(ONE)),
-            InlineKeyboardButton("3", callback_data=str(THREE)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Second CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-    )
-    return START_ROUTES
+    message = query.message
+    await message.reply_text(message)
+    bot = context.bot
+    chat_id = message.chat.id
 
+    await query.message.reply_text(message.chat)
 
-async def three(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons. This is the end point of the conversation."""
+    # if message.contact:
+    #     cpn = message.contact.phone_number
+    #     cfn = message.contact.first_name
+    #     await bot.send_message(chat_id, "#contact [{0}](https://telegram.me/{1})".format(user_id, username), parse_mode="Markdown", disable_web_page_preview=True)
+    #     await bot.send_contact(chat_id,  phone_number=cpn, first_name=cfn)
+    # if message.location:
+    #     await bot.send_message(chat_id, "#location [{0}](https://telegram.me/{1})".format(user_id, username), parse_mode="Markdown", disable_web_page_preview=True)
+    #     await bot.send_location(chat_id,  message.location.latitude, message.location.longitude)
+
+    await bot.send_photo(chat_id, "ONE.jpg", "ONE image")
+
+    # reply_markup = InlineKeyboardMarkup(
+    #     [
+    #         [
+    #             InlineKeyboardButton("Phone Number", callback_data=str(THREE)),
+    #             InlineKeyboardButton("Location", callback_data=str(FOUR)),
+    #         ]
+    #     ]
+    # )
+    # await message.reply_text(
+    #     text="What do you want to send?", reply_markup=reply_markup
+    # )
+    return END_ROUTES
+
+async def loc_usa(update, context):
+    await update.message.reply_text("Fuck USA")
+    return END_ROUTES
+
+async def loc_vietnam(update, context):
+    df = pd.read_excel("user_info")
+    print(pd)
+    df.iloc[-1, "Country"] = "Viet Nam"
+
+async def loc_other(update, context):
+    # await update.message.reply_text("👇 Please enter your country name below")
     query = update.callback_query
     await query.answer()
-    keyboard = [
+    message = query.message
+    await message.reply_text("👇 Please enter your country name below")
+    await update.message
+    message = update.message
+    await message.reply_text(message)
+
+async def location(update, context):
+    query = update.callback_query
+    await query.answer()
+    message = query.message
+    await message.reply_text(message)
+    bot = context.bot
+    chat_id = message.chat.id
+    await query.message.reply_text(message.chat)
+
+    # if message.contact:
+    #     cpn = message.contact.phone_number
+    #     cfn = message.contact.first_name
+    #     await bot.send_message(chat_id, "#contact [{0}](https://telegram.me/{1})".format(user_id, username), parse_mode="Markdown", disable_web_page_preview=True)
+    #     await bot.send_contact(chat_id,  phone_number=cpn, first_name=cfn)
+    # if message.location:
+    #     await bot.send_message(chat_id, "#location [{0}](https://telegram.me/{1})".format(user_id, username), parse_mode="Markdown", disable_web_page_preview=True)
+    #     await bot.send_location(chat_id,  message.location.latitude, message.location.longitude)
+
+    await bot.send_photo(chat_id, "ONE.jpg", "ONE image")
+
+    reply_markup = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton("Yes, let's do it again!", callback_data=str(ONE)),
-            InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(TWO)),
+            [
+                InlineKeyboardButton("Phone Number", callback_data=str(PHONE_NUMBER)),
+                InlineKeyboardButton("Location", callback_data=str(LOCATION)),
+            ]
         ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Third CallbackQueryHandler. Do want to start over?",
-        reply_markup=reply_markup,
     )
-    # Transfer to conversation state `SECOND`
+    await message.reply_text(
+        text="What do you want to send?", reply_markup=reply_markup
+    )
     return END_ROUTES
 
 
-async def four(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons"""
+async def end(update, context):
     query = update.callback_query
     await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("2", callback_data=str(TWO)),
-            InlineKeyboardButton("3", callback_data=str(THREE)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Fourth CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-    )
-    return START_ROUTES
-
-
-async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over.
-    """
-    query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(text="See you next time!")
+    await query.message.reply_text(text="See you next time!")
     return ConversationHandler.END
 
 
-from constants import TOKEN
-
-
-def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
+def main():
     application = Application.builder().token(TOKEN).build()
-
-    # Setup conversation handler with the states FIRST and SECOND
-    # Use the pattern parameter to pass CallbackQueries with specific
-    # data pattern to the corresponding handlers.
-    # ^ means "start of line/string"
-    # $ means "end of line/string"
-    # So ^ABC$ will only allow 'ABC'
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            START_ROUTES: [
-                CallbackQueryHandler(one, pattern="^" + str(ONE) + "$"),
-                CallbackQueryHandler(two, pattern="^" + str(TWO) + "$"),
-                CallbackQueryHandler(three, pattern="^" + str(THREE) + "$"),
-                CallbackQueryHandler(four, pattern="^" + str(FOUR) + "$"),
+            COUNTRIES: [
+                CallbackQueryHandler(
+                    loc_usa, pattern="USA"
+                ),
+                CallbackQueryHandler(
+                    loc_vietnam, pattern="Viet Nam"
+                ),
+                CallbackQueryHandler(
+                    loc_other, pattern="Other"
+                ),
             ],
             END_ROUTES: [
-                CallbackQueryHandler(start_over, pattern="^" + str(ONE) + "$"),
-                CallbackQueryHandler(end, pattern="^" + str(TWO) + "$"),
+                CallbackQueryHandler(end, pattern="^" + str(END) + "$"),
             ],
         },
         fallbacks=[CommandHandler("start", start)],
     )
 
-    # Add ConversationHandler to application that will be used for handling updates
     application.add_handler(conv_handler)
 
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
