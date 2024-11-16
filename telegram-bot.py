fa938174392e2ea2abe98d708de8d4606bb7f828
Jackesque <tientranquang123@gmail.com>
2024-11-16 12:57:46 +0700

    refactor countries handler

--- telegram-bot.py
+++ telegram-bot.py
@@ -10,6 +10,8 @@ from telegram.ext import (
     CallbackQueryHandler,
     CommandHandler,
     ConversationHandler,
+    MessageHandler,
+    filters,
 )
 from constants import TOKEN
 import os
@@ -21,14 +23,13 @@ filterwarnings(
 )
 
 START_ROUTES, END_ROUTES = range(2)
-COUNTRIES, PHONE_NUMBER, END = ["Countries", "Phone Number", "End"]
+COUNTRIES, OTHER, PHONE_NUMBER, END = ["Countries", "Other", "Phone Number", "End"]
 
 
 async def start(update, context):
     message = update.message
     user = message.from_user
-
-    timestamp = dt.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    timestamp = (message.date + dt.timedelta(hours=7)).strftime("%d/%m/%Y %H:%M:%S")
     user_id = str(user.id)
     username = user.username
     first_name = user.first_name
@@ -74,9 +75,7 @@ async def start(update, context):
             ]
         ]
     )
-    await message.reply_text(
-        text="What is your country?", reply_markup=reply_markup
-    )
+    await message.reply_text(text="What is your country?", reply_markup=reply_markup)
     return COUNTRIES
 
 
@@ -114,57 +113,52 @@ async def phone_number(update, context):
     # )
     return END_ROUTES
 
+
+async def countries(update, context):
+    query = update.callback_query
+    await query.answer()
+    message = query.message
+    data = query.data
+    match data:
+        case "USA":
+            await message.reply_text("Fuck USA")
+        case "Viet Nam":
+            df = pd.read_excel("users_data.xlsx")
+            print(df)
+            df.at[df.index[-1], "Country"] = data
+            df.to_excel("users_data.xlsx", index=False)
+        case "Other":
+            await message.reply_text("👇 Please enter your country name below")
+            return OTHER
+
+
 async def loc_usa(update, context):
     await update.message.reply_text("Fuck USA")
     return END_ROUTES
+
 
 async def loc_vietnam(update, context):
     df = pd.read_excel("user_info")
     print(pd)
     df.iloc[-1, "Country"] = "Viet Nam"
 
+
 async def loc_other(update, context):
-    # await update.message.reply_text("👇 Please enter your country name below")
     query = update.callback_query
     await query.answer()
     message = query.message
     await message.reply_text("👇 Please enter your country name below")
-    await update.message
-    message = update.message
-    await message.reply_text(message)
+    return OTHER
 
-async def location(update, context):
-    query = update.callback_query
-    await query.answer()
-    message = query.message
-    await message.reply_text(message)
-    bot = context.bot
-    chat_id = message.chat.id
-    await query.message.reply_text(message.chat)
 
-    # if message.contact:
-    #     cpn = message.contact.phone_number
-    #     cfn = message.contact.first_name
-    #     await bot.send_message(chat_id, "#contact [{0}](https://telegram.me/{1})".format(user_id, username), parse_mode="Markdown", disable_web_page_preview=True)
-    #     await bot.send_contact(chat_id,  phone_number=cpn, first_name=cfn)
-    # if message.location:
-    #     await bot.send_message(chat_id, "#location [{0}](https://telegram.me/{1})".format(user_id, username), parse_mode="Markdown", disable_web_page_preview=True)
-    #     await bot.send_location(chat_id,  message.location.latitude, message.location.longitude)
-
-    await bot.send_photo(chat_id, "ONE.jpg", "ONE image")
-
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Phone Number", callback_data=str(PHONE_NUMBER)),
-                InlineKeyboardButton("Location", callback_data=str(LOCATION)),
-            ]
-        ]
-    )
-    await message.reply_text(
-        text="What do you want to send?", reply_markup=reply_markup
-    )
-    return END_ROUTES
+async def loc_other_specify(update, context):
+    user_input = update.message.text
+    await update.message.reply_text(user_input)
+    df = pd.read_excel("users_data.xlsx")
+    df.at[df.index[-1], "Country"] = update.message.text
+    df.to_excel("users_data.xlsx", index=False)
+    # await update.message.reply_text(f"Your country has been set to {user_input}")
+    # return END_ROUTES
 
 
 async def end(update, context):
@@ -180,16 +174,9 @@ def main():
         entry_points=[CommandHandler("start", start)],
         states={
             COUNTRIES: [
-                CallbackQueryHandler(
-                    loc_usa, pattern="USA"
-                ),
-                CallbackQueryHandler(
-                    loc_vietnam, pattern="Viet Nam"
-                ),
-                CallbackQueryHandler(
-                    loc_other, pattern="Other"
-                ),
+                CallbackQueryHandler(countries, pattern=""),
             ],
+            OTHER: [MessageHandler(filters.TEXT, loc_other_specify)],
             END_ROUTES: [
                 CallbackQueryHandler(end, pattern="^" + str(END) + "$"),
             ],
